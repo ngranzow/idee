69da52cdf1a2b693b5c1b6d1a455c6c6f1a6c57b
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-05-30 08:01:51 -0700

    chakra ui initial structure

---
+++ index.jsx
@@ -0,0 +1,17 @@
+//CHALKRA PROVIDER SETUP
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  )
+}
+
+
+
