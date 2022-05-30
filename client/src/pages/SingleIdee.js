73728ddba7ecad27b221ca9e97cca6408eb64d2b
ngranzow <nate.granzow@gmail.com>
2022-05-30 11:21:03 -0500

    added proxy to package.json in client, and fixed some bugs on SingleIdee.js

--- client/src/pages/SingleIdee.js
+++ client/src/pages/SingleIdee.js
@@ -6,14 +6,14 @@ import IdeereplyList from '../components/IdeereplyList'
 import IdeereplyForm from '../components/IdeereplyForm';
 import Auth from '../utils/auth';
 
-const SingleThought = (props) => {
+const SingleIdee = (props) => {
   const { id: ideeId } = useParams();
 
   const { loading, data } = useQuery(QUERY_IDEE, {
     variables: { id: ideeId }
   });
 
-  const thought = data?.idee || {};
+  const idee = data?.idee || {};
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,16 +26,16 @@ const SingleThought = (props) => {
           <span style={{ fontWeight: 700 }} className="text-light">
             {idee.username}
           </span>{' '}
-          thought on {idee.createdAt}
+          idee on {idee.createdAt}
         </p>
         <div className="card-body">
           <p>{idee.ideeText}</p>
         </div>
       </div>
       {idee.replyCount > 0 && <IdeereplyList replys={idee.replys} />}
-      {Auth.loggedIn() && <IdeereplyForm replyId={reply._id} />}
+      {Auth.loggedIn() && <IdeereplyForm replyId={idee._id} />}
     </div>
   );
 };
 
-export default SingleThought;
+export default SingleIdee;
