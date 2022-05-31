82cf7c38bd583892a456516cfc5c8dc0959a2790
christine-zhe <christinez@ucla.edu>
2022-05-30 22:45:40 -0700

    edited wording and title

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -36,7 +36,8 @@ const Login = (props) => {
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
         <div className='card'>
-          <h4 className='card-header'>Login</h4>
+          <h4 className='card-header'>Login Below</h4>
+          <h5>Please use your email to login.</h5>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
               <input
